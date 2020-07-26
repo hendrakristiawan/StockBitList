071b9b7de4916dec53f89b3c319ba4526446c1b6
hendrakristiawan <hendra@jojonomic.com>
2020-07-26 11:40:50 +0700

    add base fragment

---
+++ commons/ui/src/main/java/com/hendra/ui/base/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.hendra.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+
+abstract class BaseFragment<V : ViewDataBinding, T : ViewModel>(
+    @LayoutRes
+    private val layoutId: Int
+) : Fragment() {
+
+    lateinit var viewBinding: V
+    lateinit var viewModel: T
+
+    abstract fun initDataBinding()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        viewBinding.lifecycleOwner = viewLifecycleOwner
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDataBinding()
+    }
+}
